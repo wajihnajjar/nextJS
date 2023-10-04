2fe2873b41d0eb5b64d2189bd91adbe85945f3af
wajihnajjar <wajih.najjar@esprit.tn>
2023-10-04 23:02:56 +0100

    ..

--- src/app/data/page.js
+++ src/app/data/page.js
@@ -1,5 +1,5 @@
+"use client"
 import { Suspense, useEffect, useState } from "react"
-
 async function getData (){
     const res = await fetch("https://api.publicapis.org/entries" ,{cache:'force-cache'} )
     console.log("Pulling")
@@ -10,18 +10,16 @@ export default  async function Data () {
     const [data , setData] = useState([])
     useEffect( async ()=>{
          let D = await getData()
+         console.log(D)
          setData(D[0])
         
     },[])
-     console.log(res)
     return (
-        <Suspense  fallback={<p>Loding..</p>}>
         <div>
-        {Data.map((post) => {
+        {data.map((post) => {
           return <h1>{post["API"]}</h1>
         })}
- </div>
-        </Suspense>
+        </div>
 
 )
 
